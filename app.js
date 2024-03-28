8895690dbe99828d57f17de1a3327b7dcfcd6de7
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-03-28 16:07:49 +0530

    first

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT||3000 ;
 const cors = require('cors');
 const mongoose = require('mongoose');
 
